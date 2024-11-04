0b015a1e0974819f10154d143ec065d2e76bc5e4
Ricopam <49238876+leekopam@users.noreply.github.com>
2024-11-04 19:59:41 +0900

    손님 랜덤생성기능 최적화
    제대로 작동하는거 확인함

--- Assets/Unconscious/Script/Program.cs
+++ Assets/Unconscious/Script/Program.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,22 +5,13 @@ public class Program : MonoBehaviour
 {
     [SerializeField] private Text timerText; // 타이머 값을 표시할 텍스트 UI
 
-    private CustomerManager customerManager;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        customerManager = FindObjectOfType<CustomerManager>();
+        CustomerManager customerManager = FindObjectOfType<CustomerManager>();
         if (customerManager != null)
         {
             customerManager.OnTimerUpdated += UpdateTimerText; // 타이머 업데이트 이벤트 구독
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void UpdateTimerText(float timeRemaining)
