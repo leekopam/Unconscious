e036be0a93e4a78053516517e58bf217e58944c9
Ricopam <49238876+leekopam@users.noreply.github.com>
2024-11-18 20:31:35 +0900

    오브젝트 배치
    술병 배치됨

--- Assets/Unconscious/Script/Cocktail/New Folder/AlcoholDatabase.cs
+++ Assets/Unconscious/Script/Cocktail/New Folder/AlcoholDatabase.cs
@@ -7,30 +7,27 @@ public static class AlcoholDatabase
     {
         return new List<Alcohol>
         {
-            // 1번 칵테일
             new Alcohol
             {
-                name = "1번 칵테일",
+                name = "1번 술",
                 alcoholContent = 8,    // 높음
                 bitterness = 5,        // 보통
                 sweetness = 6,         // 조금 달콤
                 aroma = Aroma.fruity,  // 과일향
                 aromaIntensity = 4     // 연한
             },
-            // 재료 1
             new Alcohol
             {
-                name = "재료 1",
+                name = "2번 술",
                 alcoholContent = 6,    // 보통
                 bitterness = 5,        // 보통
                 sweetness = 10,        // 매우 달콤
                 aroma = Aroma.fruity,  // 과일향
                 aromaIntensity = 9     // 진한
             },
-            // 재료 2
             new Alcohol
             {
-                name = "재료 2",
+                name = "3번 술",
                 alcoholContent = 10,   // 매우 강함
                 bitterness = 5,        // 보통
                 sweetness = 2,         // 약간 달콤
