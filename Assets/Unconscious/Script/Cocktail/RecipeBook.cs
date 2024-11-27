d7a02d8fc9f7be7785711751ff9182cd757a4609
Ricopam <49238876+leekopam@users.noreply.github.com>
2024-11-27 16:02:19 +0900

    흔들기, 젓기, 쌓기 기능 추가
    버튼으로 상태를 전달하고 동시에 계산도 같이 하도록 버튼에 할당함

--- Assets/Unconscious/Script/Cocktail/RecipeBook.cs
+++ Assets/Unconscious/Script/Cocktail/RecipeBook.cs
@@ -5,47 +5,61 @@ public enum Recipe
     연홍남자,
     그린나래,
     경성,
-    한양의_일출,
+    한양의일출,
     명경지수,
-    풀_빛_아리아,
+    풀빛아리아,
    정체불명의음료
+}
+
+public enum MixState
+{
+    Shake, //흔들기
+    Stir, //젓기
+    Layer //쌓기
 }
 
 public class RecipeBook : MonoBehaviour
 {
     public Recipe? Check_Cocktail_Recipe(
         string name1, int alcoholContent1, int sweetness1, int bitterness1, FlavorType flavorType1, int flavorIntensity1,
-        string name2, int alcoholContent2, int sweetness2, int bitterness2, FlavorType flavorType2, int flavorIntensity2)
+        string name2, int alcoholContent2, int sweetness2, int bitterness2, FlavorType flavorType2, int flavorIntensity2
+        ,MixState mixstate)
     {
         // 연홍남자 = 도화주 + 나린 이화주
-        if ((name1 == "도화주" && name2 == "나린 이화주") || (name1 == "나린 이화주" && name2 == "도화주"))
+        if ((mixstate==MixState.Shake&&
+            name1 == "도화주" && name2 == "나린 이화주") || (name1 == "나린 이화주" && name2 == "도화주"))
         {
             return Recipe.연홍남자;
         }
         // 그린나래 = 가향주 + 소슬바람
-        if ((name1 == "가향주" && name2 == "소슬바람") || (name1 == "소슬바람" && name2 == "가향주"))
+        if (mixstate == MixState.Stir && 
+            (name1 == "가향주" && name2 == "소슬바람") || (name1 == "소슬바람" && name2 == "가향주"))
         {
             return Recipe.그린나래;
         }
         //경성 = 나린 이화주 + 해류 뭄해리
-        if ((name1 == "나린 이화주" && name2 == " 해류 뭄해리") || (name1 == " 해류 뭄해리" && name2 == "나린 이화주"))
+        if ((mixstate == MixState.Layer && 
+            name1 == "나린 이화주" && name2 == " 해류 뭄해리") || (name1 == " 해류 뭄해리" && name2 == "나린 이화주"))
         {
             return Recipe.경성;
         }
         //한양의_일출 = 소슬바람 + 영반월
-        if ((name1 == "소슬바람" && name2 == "영반월") || (name1 == "영반월" && name2 == "소슬바람"))
+        if ((mixstate == MixState.Shake && 
+            name1 == "소슬바람" && name2 == "영반월") || (name1 == "영반월" && name2 == "소슬바람"))
         {
-            return Recipe.한양의_일출;
+            return Recipe.한양의일출;
         }
         //명경지수 = 도화주 + 영반월
-        if ((name1 == "도화주" && name2 == "영반월") || (name1 == "영반월" && name2 == "도화주"))
+        if ((mixstate == MixState.Stir && 
+            name1 == "도화주" && name2 == "영반월") || (name1 == "영반월" && name2 == "도화주"))
         {
             return Recipe.명경지수;
         }
         //풀_빛_아리아 = 가향주 + 해류 뭄해리
-        if ((name1 == "가향주" && name2 == "해류 뭄해리") || (name1 == "해류 뭄해리" && name2 == "가향주"))
+        if ((mixstate == MixState.Layer && 
+            name1 == "가향주" && name2 == "해류 뭄해리") || (name1 == "해류 뭄해리" && name2 == "가향주"))
         {
-            return Recipe.풀_빛_아리아;
+            return Recipe.풀빛아리아;
         }
         else
         {
