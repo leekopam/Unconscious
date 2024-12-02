9e030160645e7db7853c4debeb129ababa74f6e3
Ricopam <49238876+leekopam@users.noreply.github.com>
2024-12-02 14:55:32 +0900

    벨 애니메이션 기능 추가
    벨 클릭하면 애니메이션 작동하게 기능추가
    - 빈 오브젝트에 Rigdbody추가 Is Kinematic기능 True로 설정, 하위 오브젝트에(3d모델)에 콜라이더 붙여서 사용

---
+++ Assets/Unconscious/Script/Topping/Bell.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Bell : MonoBehaviour
+{
+   private PlayableDirector playableDirector;
+    private bool isPlay = false;
+
+    private void Start()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+
+        //애니메이션 완료 이벤트 리스너 추가
+        playableDirector.stopped += OnPlayableDirectorStopped;
+    }
+    private void OnPlayableDirectorStopped(PlayableDirector playable)
+    {
+        //애니메이션 완료 후 처리
+        isPlay = false;
+    }
+
+    private void OnDestroy()
+    {
+        //애니메이션 완료 이벤트 리스너 제거
+        playableDirector.stopped -= OnPlayableDirectorStopped;
+    }
+
+    //마우스 클릭했을때 애니메이션 재생
+    private void OnMouseDown()
+    {
+        if (!isPlay)
+        {
+            PlayAnimation();
+        }
+    }
+
+    private void PlayAnimation()
+    {
+        //애니메이션 재생
+        playableDirector.Play();
+        isPlay = true;
+    }
+}
+
+
