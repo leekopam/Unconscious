eb362ec37cefeb9b53ee84ad2674eb93e66bdceb
Ricopam <49238876+leekopam@users.noreply.github.com>
2024-12-06 23:17:37 +0900

    페이드 인/아웃 수정
    페이드 인/아웃 기능만 들어있는 캔버스를 하나의 프리팹으로 만들어서 할당

--- Assets/Unconscious/Script/System/FadeManager.cs
+++ Assets/Unconscious/Script/System/FadeManager.cs
@@ -5,13 +5,40 @@ using System.Collections;
 
 public class FadeManager : MonoBehaviour
 {
+    public static FadeManager Instance { get; private set; }
     public Image fadeImage;
-    public GameObject fadeImageOBJ;
     public float fadeTime = 1f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            StartCoroutine(FadeInOnStart());
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator FadeInOnStart()
+    {
+        fadeImage.color = new Color(0f, 0f, 0f, 1f);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeTime);
+            fadeImage.color = new Color(0f, 0f, 0f, alpha);
+            yield return null;
+        }
+    }
 
     public void FadeAndLoadScene(string sceneName)
     {
-        if (fadeImageOBJ.activeSelf == false) { fadeImageOBJ.SetActive(true); }
         StartCoroutine(FadeSequence(sceneName));
     }
 
@@ -27,17 +54,7 @@ public class FadeManager : MonoBehaviour
             yield return null;
         }
 
-        // 씬 로드
         SceneManager.LoadScene(sceneName);
-
-        // 페이드 인
-        elapsedTime = 0f;
-        while (elapsedTime < fadeTime)
-        {
-            elapsedTime += Time.deltaTime;
-            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeTime);
-            fadeImage.color = new Color(0f, 0f, 0f, alpha);
-            yield return null;
-        }
+        StartCoroutine(FadeInOnStart());
     }
 }
