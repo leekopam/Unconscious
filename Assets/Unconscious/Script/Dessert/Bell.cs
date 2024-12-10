9a96c772276ff020a1a21b191fc000a75f1abd93
Ricopam <49238876+leekopam@users.noreply.github.com>
2024-12-10 18:12:08 +0900

    벨 누르면 씬 전환 기능추가

--- Assets/Unconscious/Script/Dessert/Bell.cs
+++ Assets/Unconscious/Script/Dessert/Bell.cs
@@ -1,31 +1,37 @@
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class Bell : MonoBehaviour
 {
-   private PlayableDirector playableDirector;
+    private PlayableDirector playableDirector;
     private bool isPlay = false;
 
     private void Start()
     {
         playableDirector = GetComponent<PlayableDirector>();
 
-        //애니메이션 완료 이벤트 리스너 추가
+        // 애니메이션 완료 이벤트 리스너 추가
         playableDirector.stopped += OnPlayableDirectorStopped;
     }
+
     private void OnPlayableDirectorStopped(PlayableDirector playable)
     {
-        //애니메이션 완료 후 처리
-        isPlay = false;
+        // 애니메이션 완료 후 씬 전환
+        if (isPlay)
+        {
+            SceneManager.LoadScene("Order");
+            isPlay = false;
+        }
     }
 
     private void OnDestroy()
     {
-        //애니메이션 완료 이벤트 리스너 제거
+        // 애니메이션 완료 이벤트 리스너 제거
         playableDirector.stopped -= OnPlayableDirectorStopped;
     }
 
-    //마우스 클릭했을때 애니메이션 재생
+    // 마우스 클릭했을때 애니메이션 재생
     private void OnMouseDown()
     {
         if (!isPlay)
@@ -36,10 +42,8 @@ public class Bell : MonoBehaviour
 
     private void PlayAnimation()
     {
-        //애니메이션 재생
+        // 애니메이션 재생
         playableDirector.Play();
         isPlay = true;
     }
 }
-
-
